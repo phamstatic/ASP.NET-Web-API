1c473b36286fd453dca6c24b292e52d08a1601a3
John Pham <john.pham@omega365.com>
2024-12-16 15:08:19 -0600

    Installed Entity Framework packages

--- EmployeeAdminPortal/EmployeeAdminPortal/obj/Debug/net8.0/EmployeeAdminPortal.AssemblyInfo.cs
+++ EmployeeAdminPortal/EmployeeAdminPortal/obj/Debug/net8.0/EmployeeAdminPortal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeAdminPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b4bbdd6f1ce7489d2602466d5374e2911d765a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d08120a5b9c5cec4651a6ff486b122aae62a34b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeAdminPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeAdminPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
